fd7fe438ff9b65b36a5d271f771bae7d9a4f33d8
Jonathan Carroll Otsuka <105506+djgoku@users.noreply.github.com>
2026-03-22 18:21:33 -0500

    chore: add GPL headers, full author metadata, and processes keyword
    Add GPL-3.0-or-later license headers to both source files.
    Update Author to full name with email. Change Keywords from
    compilation to processes (standard finder keyword) for MELPA.

--- compilation-history-view.el
+++ compilation-history-view.el
@@ -1,14 +1,28 @@
 ;;; compilation-history-view.el --- View compilation history -*- lexical-binding: t; -*-
 
-;; Copyright (C) 2025 Jonathan Otsuka
+;; Copyright (C) 2025 Jonathan Carroll Otsuka
 
-;; Author: Jonathan Otsuka
+;; Author: Jonathan Carroll Otsuka <pitas.axioms0c@icloud.com>
 ;; Version: 0.1.0
 ;; Package-Requires: ((emacs "29.1"))
-;; Keywords: tools
+;; Keywords: processes, tools
 ;; URL: https://github.com/djgoku/compilation-history
 
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
 ;;; Commentary:
+
 ;; Paginated vtable-based UI for browsing compilation history records.
 
 ;;; Code:
--- compilation-history.el
+++ compilation-history.el
@@ -1,12 +1,25 @@
-;;; compilation-history.el --- Track compilation history -*- lexical-binding: t; -*-
+;;; compilation-history.el --- Track compilation history in SQLite -*- lexical-binding: t; -*-
 
-;; Copyright (C) 2025
+;; Copyright (C) 2025 Jonathan Carroll Otsuka
 
-;; Author: djgoku
+;; Author: Jonathan Carroll Otsuka <pitas.axioms0c@icloud.com>
 ;; Version: 0.1.0
 ;; Package-Requires: ((emacs "29.1"))
-;; Keywords: compilation, tools, history
+;; Keywords: processes, tools
 ;; URL: https://github.com/djgoku/compilation-history
+
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
 ;;; Commentary:
 
